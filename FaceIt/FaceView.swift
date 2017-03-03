dcdc641ad772fa3817ff22cca0af5e3a5d6f598a
Lennart Wisbar <anlennart@gmail.com>
2017-03-03 08:46:29 +0100

    Draws a circle.

---
+++ FaceIt/FaceView.swift
@@ -0,0 +1,34 @@
+//
+//  FaceView.swift
+//  FaceIt
+//
+//  Created by Lennart Wisbar on 03.03.17.
+//  Copyright © 2017 Lennart Wisbar. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    var scale: CGFloat = 0.9
+    
+    private var skullRadius: CGFloat {
+        return min(bounds.width, bounds.height) / 2 * scale
+    }
+    
+    private var skullCenter: CGPoint {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    
+    private func pathForSkull() -> UIBezierPath {
+        let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        path.lineWidth = 5.0
+        return path
+    }
+    
+    override func draw(_ rect: CGRect) {
+        UIColor.blue.set()
+        pathForSkull().stroke()
+    }
+
+}
